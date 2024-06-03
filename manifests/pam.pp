ad5265613c8b8eb1f00d44021688f1982ab68883
Bill Glick <bglick@illinois.edu>
2024-06-03 15:14:02 -0500

    SVCPLAN-5555: Add support for SUSE, upgrade PDK

--- manifests/pam.pp
+++ manifests/pam.pp
@@ -7,6 +7,9 @@ class pam_access::pam {
   $_os_ver = Integer.new( $facts['os']['release']['major'] )
 
   case $facts['os']['family'] {
+    'Debian': {
+      contain pam_access::pam::debian
+    }
     'RedHat': {
       if $_os_ver <= 7 {
         contain pam_access::pam::redhat7
@@ -14,8 +17,8 @@ class pam_access::pam {
         contain pam_access::pam::redhat
       }
     }
-    'Debian': {
-      contain pam_access::pam::debian
+    'Suse': {
+      contain pam_access::pam::suse
     }
     default: {}
   }
---
+++ manifests/pam/suse.pp
@@ -0,0 +1,37 @@
+# @summary Implement pam_access on SUSE systems
+#
+# @param authselect_features
+#   Valid only for SUSE/SLES
+#
+#   Default:
+#   * `with-pamaccess`
+#
+class pam_access::pam::suse (
+  Array[String] $authselect_features,
+) {
+
+  # SUSE use authselect
+
+  $_ensure = lookup( 'pam_access::ensure' )
+
+  # Only do work if a profile is active
+  $_profile = $facts['authselect']['profile']
+  if $facts['authselect']['profile'] =~ String[1] {
+
+    $authselect_features.each | $feature_name | {
+      if $feature_name in $facts['authselect']['features'] {
+        if $_ensure == 'absent' {
+          # feature is present but should be absent
+          exec { "/usr/bin/authselect disable-feature ${feature_name}": }
+        }
+      } else {
+        if $_ensure == 'present' {
+          # feature is not present but should be
+          exec { "/usr/bin/authselect enable-feature ${feature_name}": }
+        }
+      }
+    }
+
+  }
+
+}
